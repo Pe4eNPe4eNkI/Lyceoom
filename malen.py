ca4265a5d346c8d8893ec263211a9d32403600e2
Arseniy <av59ru@yandex.ru>
2021-01-02 21:05:17 +0500

    malen.1.5.3

--- malen.py
+++ malen.py
@@ -3,6 +3,7 @@ from parameters import *
 from r_c import ray_casting
 from map import mini_map
 
+
 # для коммита
 # еще каммит
 
@@ -28,8 +29,8 @@ class Malen:
     def mini_map(self, gamer):
         self.monitor_map.fill(BLACK)
         map_x, map_y = self.gamer.x // MAP_SCALE, self.gamer.y // MAP_SCALE
-        pygame.draw.line(self.monitor_map, YELLOW, (map_x, map_y), (map_x + 80 * math.cos(self.gamer.angle),
-                                                                    map_y + 80 * math.sin(self.gamer.angle)), 2)
+        pygame.draw.line(self.monitor_map, YELLOW, (map_x, map_y), (map_x + 40 * math.cos(self.gamer.angle),
+                                                                    map_y + 40 * math.sin(self.gamer.angle)), 2)
         pygame.draw.circle(self.monitor_map, RED, (int(map_x), int(map_y)), 4)
 
         for x, y in mini_map:
