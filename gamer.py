d43f987317fbc0dda107d3fe1e45acdbab411e90
feelsbadkot <bond1010a@gmail.com>
2021-01-02 12:24:13 +0500

    Добавили управление для игрока

---
+++ gamer.py
@@ -0,0 +1,31 @@
+import pygame
+from parameters import *
+from main import terminate
+
+
+class Gamer:
+    def __init__(self, sprites):
+        self.x, self.y = gamer_pos
+        self.angle = gamer_angle
+        self.sensitivity = 0.004
+
+    def pos(self):
+        return (self.x, self.y)
+
+    def movement(self):
+        keys = pygame.key.get_pressed()
+        if keys[pygame.K_ESCAPE]:
+            terminate()
+        if keys[pygame.K_w]:
+            self.y -= gamer_speed
+        if keys[pygame.K_s]:
+            self.y += gamer_speed
+        if keys[pygame.K_a]:
+            self.x -= gamer_speed
+        if keys[pygame.K_d]:
+            self.x += gamer_speed
+        if keys[pygame.K_LEFT]:
+            self.angle -= 0.02
+        if keys[pygame.K_RIGHT]:
+            self.angle += 0.02
+        
