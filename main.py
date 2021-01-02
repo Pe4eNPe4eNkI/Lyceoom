6e20ef119ef9c32a3a56b9e151811aa86ca2e03d
BelkovskiDaniil <belka.danya1939@mail.ru>
2021-01-02 11:53:00 +0500

    Подготовка к пробному запуску 2D main

--- main.py
+++ main.py
@@ -1,13 +1,21 @@
 import pygame
 from parameters import *
+from gamer import Gamer
 
 
 pygame.init()
 monitor = pygame.display.set_mode((WIDHT, HIGHT))
 timer = pygame.time.Clock()
+gamer = Gamer()
 
 while True:
     for event in pygame.event.get():
         if pygame.event == pygame.QUIT:
             exit()
+    gamer.movement()
     monitor.fill(BLACK)
+
+    pygame.draw.rect(monitor, RED, (gamer.pos[0], gamer.pos[1], 6, 6))
+
+    pygame.display.flip()
+    timer.tick()
