17cba23bed1cda6fbeda38726b65c0dd7a0600ee
BelkovskiDaniil <belka.danya1939@mail.ru>
2021-01-02 12:22:25 +0500

    Подготовка к пробному запуску 2D main (исправление ошибок)

--- main.py
+++ main.py
@@ -1,27 +1,28 @@
 import pygame
 import sys
 from parameters import *
-'''from gamer import Gamer'''
+from gamer import Gamer
+
+
+def terminate():
+    pygame.quit()
+    sys.exit()
 
 
 pygame.init()
 monitor = pygame.display.set_mode((WIDTH, HEIGHT))
 timer = pygame.time.Clock()
-'''gamer = Gamer()'''
+gamer = Gamer()
+
 
 while True:
     for event in pygame.event.get():
         if pygame.event == pygame.QUIT:
             terminate()
-    '''gamer.movement()'''
+    gamer.movement()
     monitor.fill(BLACK)
 
     pygame.draw.rect(monitor, RED, (gamer_pos[0], gamer_pos[1], 6, 6))
 
     pygame.display.flip()
     timer.tick(FPS)
-
-
-def terminate():
-    pygame.quit()
-    sys.exit()
