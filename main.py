db59fdeda33935cb4650ff2dbbd2da9cdde302c5
BelkovskiDaniil <belka.danya1939@mail.ru>
2021-01-02 18:28:13 +0500

    Малые исправления

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ from parameters import *
 from gamer import Gamer
 from map import txt_map
 from r_c import ray_casting
+from malen import Malen
 
 
 def terminate():
@@ -16,6 +17,7 @@ pygame.init()
 monitor = pygame.display.set_mode((WIDTH, HEIGHT))
 timer = pygame.time.Clock()
 gamer = Gamer()
+malen = Malen(monitor)
 
 while True:
     for event in pygame.event.get():
@@ -23,11 +25,8 @@ while True:
             terminate()
     gamer.movement()
     monitor.fill(BLACK)
-    pygame.draw.rect(monitor, BLUE, (0, 0, WIDTH, H_HEIGHT))
-    pygame.draw.rect(monitor, DARKGREY, (0, H_HEIGHT, WIDTH, H_HEIGHT))
 
     ray_casting(monitor, gamer.pos, gamer.angle)
-
     '''pygame.draw.circle(monitor, RED, (int(gamer.x), int(gamer.y)), 12)
     pygame.draw.line(monitor, RED, gamer.pos,
                      (gamer.x + WIDTH * math.cos(gamer.angle), gamer.y + WIDTH * math.sin(gamer.angle)))
@@ -36,8 +35,6 @@ while True:
 
     for x, y in txt_map:
         pygame.draw.rect(monitor, DARKGREY, (x, y, CELL, CELL), 2)'''
-
-
 
     pygame.display.flip()
     timer.tick(FPS)
--- r_c.py
+++ r_c.py
@@ -12,12 +12,11 @@ def ray_casting(monitor, gamer_pos, gamer_angle):
         for i in range(MAX_DEPTH):
             x = ox + i * cos_a
             y = oy + i * sin_a
-            '''pygame.draw.line(monitor, DARKGREY, gamer_pos, (x, y), 2)'''
             if (x // CELL * CELL, y // CELL * CELL) in txt_map:
                 i *= math.cos(gamer_angle - view_angle)
                 hight = PROJ_C / i
                 a = 255 / (1 + i * i * 0.0001)
-                color = (a // 3, a  // 5, a // 2)
+                color = (a, a, a)
                 pygame.draw.rect(monitor, color, (ray * SCALE, H_HEIGHT - hight // 2, SCALE, hight))
                 break
         view_angle += DELTA_ANGLE
