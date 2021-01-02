4ba24778e61bf03e05699ab3437107dae2df1112
BelkovskiDaniil <belka.danya1939@mail.ru>
2021-01-02 18:38:41 +0500

    Merge remote-tracking branch 'origin/main' into main
    # Conflicts:
    #	main.py

--- main.py
+++ main.py
@@ -5,7 +5,6 @@ from parameters import *
 from gamer import Gamer
 from map import txt_map
 from r_c import ray_casting
-from malen import Malen
 
 
 def terminate():
@@ -25,8 +24,15 @@ while True:
             terminate()
     gamer.movement()
     monitor.fill(BLACK)
+    malen.background()
+    malen.world(player.pos, player.angle)
+    malen.fps(clock)
+    '''pygame.draw.rect(monitor, BLUE, (0, 0, WIDTH, H_HEIGHT))
+    pygame.draw.rect(monitor, DARKGREY, (0, H_HEIGHT, WIDTH, H_HEIGHT))'''
+
 
     ray_casting(monitor, gamer.pos, gamer.angle)
+
     '''pygame.draw.circle(monitor, RED, (int(gamer.x), int(gamer.y)), 12)
     pygame.draw.line(monitor, RED, gamer.pos,
                      (gamer.x + WIDTH * math.cos(gamer.angle), gamer.y + WIDTH * math.sin(gamer.angle)))
@@ -35,6 +41,8 @@ while True:
 
     for x, y in txt_map:
         pygame.draw.rect(monitor, DARKGREY, (x, y, CELL, CELL), 2)'''
+
+
 
     pygame.display.flip()
     timer.tick(FPS)
