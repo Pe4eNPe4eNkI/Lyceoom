656843b571eed2cafd997f530c9680f9b2a86470
Arseniy <av59ru@yandex.ru>
2021-01-04 20:34:19 +0500

    fix update 2.4

---
+++ main.py
@@ -0,0 +1,40 @@
+import pygame
+import sys
+from parameters import *
+from gamer import Gamer
+from sprites import *
+from r_c import walls_with_ray_cast
+from malen import Malen
+
+
+def terminate():
+    pygame.quit()
+    sys.exit()
+
+
+pygame.init()
+monitor = pygame.display.set_mode((WIDTH, HEIGHT))
+mon_map = pygame.Surface(MAP_RES)
+pygame.mouse.set_visible(False)
+timer = pygame.time.Clock()
+sprites = Sprites()
+gamer = Gamer(sprites)
+sprite = Sprites()
+malen = Malen(monitor, mon_map, gamer)
+
+while True:
+    for event in pygame.event.get():
+        if pygame.event == pygame.QUIT:
+            terminate()
+    gamer.movement()
+    monitor.fill(BLACK)
+    malen.bg(gamer.angle)
+    walls = walls_with_ray_cast(gamer, malen.texture)
+    malen.world(walls + [obj.object_locate(gamer, walls) for obj in sprite.list_of_sprites])
+    malen.fps(timer)
+    malen.mini_map(gamer)
+
+    pygame.display.flip()
+    timer.tick(FPS)
+
+# tests
---
+++ parameters.py
@@ -0,0 +1,62 @@
+# здесь хранятся всякие разные константы для остальных файлов проекта
+import math
+
+# общие настройки
+WIDTH = 1200
+HEIGHT = 800
+H_HEIGHT = HEIGHT / 2
+H_WIDTH = WIDTH / 2
+D_HEIGHT = HEIGHT * 2
+D_WIDTH = WIDTH * 2
+P_HEIGHT = 5 * HEIGHT
+CELL = 100
+FPS = 90
+FPS_POS = (WIDTH - 65, 5)
+
+# настройки игрока
+gamer_pos = (H_WIDTH, H_HEIGHT)
+gamer_angle = 0
+gamer_speed = 2.5
+
+# настройки лучей
+FOV = math.pi / 3
+H_FOV = FOV / 2
+N_RAYS = 300
+MAX_DEPTH = 800
+DELTA_ANGLE = FOV / N_RAYS
+DISTANCE = N_RAYS / (2 * math.tan(H_FOV))
+PROJ_C = 3 * DISTANCE * CELL
+SCALE = WIDTH // N_RAYS
+
+# настройки для миникарты
+
+MINIMAP_SCALE = 5
+MAP_RES = (WIDTH // MINIMAP_SCALE, HEIGHT // MINIMAP_SCALE)
+MAP_SCALE = 2 * MINIMAP_SCALE
+MAP_CELL = CELL // MAP_SCALE
+MAP_POS = (0, 0)
+
+# текстуры
+T_W = 1200
+T_H = 1200
+T_SCALE = T_W // CELL
+
+# спрайты
+ZWEI_PI = math.pi * 2
+C_RAY = N_RAYS // 2 - 1  # аоаоаоаоа
+
+# цвета
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+RED = (220, 0, 0)
+GREEN = (0, 80, 0)
+BLUE = (0, 0, 220)
+DARKGREY = (61, 61, 61)
+DARKBROWN = (63, 42, 20)
+DARKORANGE = (255, 98, 0)
+PURPLE = (120, 0, 120)
+SKY_BLUE = (0, 180, 255)
+YELLOW = (220, 220, 0)
+SANDY = (244, 164, 96)
+
+# tests
