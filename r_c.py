8f2d656a57cb9bdb541d37b9c14b1a55b90d6db3
Arseniy <69422628+Pe4eNPe4eNkI@users.noreply.github.com>
2021-01-05 23:49:30 +0500

    version 0.4

--- r_c.py
+++ r_c.py
@@ -1,20 +1,21 @@
 import pygame
 from parameters import *
-from map import txt_map, H_WORLD, W_WORLD
+from numba import njit
 #from new_map import txt_map, H_WORLD, W_WORLD
+from map import txt_map, H_WORLD, W_WORLD
 
-
-
+@njit(fastmath=True)
 def mapping(a, b):
     return int((a // CELL) * CELL), int((b // CELL) * CELL)
 
 
-def ray_casting(gamer, textures):
+@njit(fastmath=True)
+def ray_casting(pos_gamer, angle_gamer, txt_map):
     walls = []
-    ox, oy = gamer.pos
+    ox, oy = pos_gamer
     texture_v, texture_h = 1, 1
     xm, ym = mapping(ox, oy)
-    view_angle = gamer.angle - H_FOV
+    view_angle = angle_gamer - H_FOV
     for ray in range(N_RAYS):
         sin_a = math.sin(view_angle)
         cos_a = math.cos(view_angle)
@@ -39,17 +40,36 @@ def ray_casting(gamer, textures):
                 break
             y += dy * CELL
 
-        depth, offset, texture = (depth_v, yv, texture_v) if depth_v < depth_h else (depth_h, xh, texture_h)
+        if depth_v < depth_h:
+            depth, offset, texture = (depth_v, yv, texture_v)
+        else:
+            depth, offset, texture = (depth_h, xh, texture_h)
         offset = int(offset) % CELL
-        depth *= math.cos(gamer.angle - view_angle)
+        depth *= math.cos(angle_gamer - view_angle)
         depth = max(depth, 0.00001)
-        hight = min((PROJ_C / depth), P_HEIGHT)
+        hight = int(PROJ_C / depth)
 
-
-        wall_c = textures[texture].subsurface(offset * T_SCALE, 0, T_SCALE, T_H)
-        wall_c = pygame.transform.scale(wall_c, (int(SCALE), int(hight)))
-        walls_pos = (ray * SCALE, H_HEIGHT - hight // 2)
-
-        walls.append((depth, wall_c, walls_pos))
+        walls.append((depth, offset, hight, texture))
         view_angle += DELTA_ANGLE
     return walls
+
+
+def walls_with_ray_cast(gamer, textures):
+    verific_walls = ray_casting(gamer.pos, gamer.angle, txt_map)
+    wall_shot = verific_walls[C_RAY][0], verific_walls[C_RAY][2]
+    walls = []
+    for ray, casted_values in enumerate(verific_walls):
+        depth, offset, hight, texture = casted_values
+        if hight > HEIGHT:
+            coeff = hight / HEIGHT
+            texture_height = T_H / coeff
+            wall_c = textures[texture].subsurface(offset * T_SCALE, H_T_H - texture_height // 2,
+                                                  T_SCALE, texture_height)
+            wall_c = pygame.transform.scale(wall_c, (SCALE, HEIGHT))
+            walls_pos = (ray * SCALE, 0)
+        else:
+            wall_c = textures[texture].subsurface(offset * T_SCALE, 0, T_SCALE, T_H)
+            wall_c = pygame.transform.scale(wall_c, (SCALE, hight))
+            walls_pos = (ray * SCALE, H_HEIGHT - hight // 2)
+        walls.append((depth, wall_c, walls_pos))
+    return walls, wall_shot
