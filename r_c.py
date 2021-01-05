bf407d19e27ed279cd95df1d27b9429596833c11
Arseniy <69422628+Pe4eNPe4eNkI@users.noreply.github.com>
2021-01-05 23:02:28 +0500

    fix 2.5

--- r_c.py
+++ r_c.py
@@ -1,9 +1,7 @@
 import pygame
 from parameters import *
-from map import txt_map, H_WORLD, W_WORLD
 from numba import njit
-#from new_map import txt_map, H_WORLD, W_WORLD
-
+from new_map import txt_map, H_WORLD, W_WORLD
 
 @njit(fastmath=True)
 def mapping(a, b):
@@ -40,6 +38,7 @@ def ray_casting(pos_gamer, angle_gamer, txt_map):
                 texture_h = txt_map[tile_h]
                 break
             y += dy * CELL
+
         if depth_v < depth_h:
             depth, offset, texture = (depth_v, yv, texture_v)
         else:
@@ -47,7 +46,7 @@ def ray_casting(pos_gamer, angle_gamer, txt_map):
         offset = int(offset) % CELL
         depth *= math.cos(angle_gamer - view_angle)
         depth = max(depth, 0.00001)
-        hight = min((PROJ_C / depth), P_HEIGHT)
+        hight = int(PROJ_C / depth)
 
         walls.append((depth, offset, hight, texture))
         view_angle += DELTA_ANGLE
@@ -56,13 +55,20 @@ def ray_casting(pos_gamer, angle_gamer, txt_map):
 
 def walls_with_ray_cast(gamer, textures):
     verific_walls = ray_casting(gamer.pos, gamer.angle, txt_map)
+    wall_shot = verific_walls[C_RAY][0], verific_walls[C_RAY][2]
     walls = []
     for ray, casted_values in enumerate(verific_walls):
         depth, offset, hight, texture = casted_values
-        wall_c = textures[texture].subsurface(offset * T_SCALE, 0, T_SCALE, T_H)
-        wall_c = pygame.transform.scale(wall_c, (int(SCALE), int(hight)))
-        walls_pos = (ray * SCALE, H_HEIGHT - hight // 2)
+        if hight > HEIGHT:
+            coeff = hight / HEIGHT
+            texture_height = T_H / coeff
+            wall_c = textures[texture].subsurface(offset * T_SCALE, H_T_H - texture_height // 2,
+                                                  T_SCALE, texture_height)
+            wall_c = pygame.transform.scale(wall_c, (SCALE, HEIGHT))
+            walls_pos = (ray * SCALE, 0)
+        else:
+            wall_c = textures[texture].subsurface(offset * T_SCALE, 0, T_SCALE, T_H)
+            wall_c = pygame.transform.scale(wall_c, (SCALE, hight))
+            walls_pos = (ray * SCALE, H_HEIGHT - hight // 2)
         walls.append((depth, wall_c, walls_pos))
-    return walls
-
-# tests
+    return walls, wall_shot
